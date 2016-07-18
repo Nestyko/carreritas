c6d1f304815b6d729eb6a010624cafd517e037fb
Nestor Tobon <nltobon@gmail.com>
2016-07-18 12:22:55 -0400

    goning on github page

--- system-config.js
+++ system-config.js
@@ -64,4 +64,4 @@ System.config({
 });
 // Apply the user's configuration.
 System.config({ map: map, packages: packages });
-//# sourceMappingURL=tmp\broccoli_type_script_compiler-input_base_path-sK8zLfQP.tmp\0\src\system-config.js.map
+//# sourceMappingURL=tmp\broccoli_type_script_compiler-input_base_path-tqEKgKVl.tmp\0\src\system-config.js.map
