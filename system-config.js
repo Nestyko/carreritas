5cbed0bd30ed519843c49fe7e6a7ec75f1f214e6
Nestor Tobon <nltobon@gmail.com>
2016-07-18 23:17:24 -0400

    new gh-pages version

--- system-config.js
+++ system-config.js
@@ -48,6 +48,7 @@ var barrels = [
     'app/corredor',
     'app/timer',
     'app/corredor-detail',
+    'app/clock',
 ];
 var cliSystemConfigPackages = {};
 barrels.forEach(function (barrelName) {
@@ -64,4 +65,4 @@ System.config({
 });
 // Apply the user's configuration.
 System.config({ map: map, packages: packages });
-//# sourceMappingURL=tmp\broccoli_type_script_compiler-input_base_path-WCOc8e21.tmp\0\src\system-config.js.map
+//# sourceMappingURL=tmp\broccoli_type_script_compiler-input_base_path-kqHnwx9L.tmp\0\src\system-config.js.map
